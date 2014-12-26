1ee9ee1faf3816771a8b773fcdf5b3c722ee32a5
jfeng0927 <jfeng@seawolftech.com>
2014-12-26 18:03:19 -0500

    add one
    minimum depth of binary tree

---
+++ src/leetCode/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package leetCode;
+
+/**
+ * @author jianyu
+ *
+ */
+public class MinimumDepthOfBinaryTree {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
