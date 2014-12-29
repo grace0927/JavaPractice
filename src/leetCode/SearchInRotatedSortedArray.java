dacb0cfca15c96bd01543652a466893374980c26
jfeng0927 <jfeng@seawolftech.com>
2014-12-29 15:11:56 -0500

    add one
    search in rotated sorted array

---
+++ src/leetCode/SearchInRotatedSortedArray.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package leetCode;
+
+/**
+ * @author jianyu
+ * https://oj.leetcode.com/problems/search-in-rotated-sorted-array/
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
+ *
+ */
+public class SearchInRotatedSortedArray {
+    public int search(int[] A, int target) {
+        if(A.length == 0) {
+			return -1;
+		}
+		
+		int start = 0;
+		int end = A.length - 1;
+		if(A[start] == target) {
+			return start;
+		}
+		if(A[end] == target) {
+			return end;
+		}
+		// binary search
+		while(end > start+1) {
+			int mid = (start+end)/2;
+			if(A[mid] == target) {
+				return mid;
+			} else if(A[mid] > target) {
+				if(target > A[start]) {
+					end = mid;
+				} else {
+					if(A[mid] > A[start]) {
+						start = mid;
+					} else {
+						end = mid;
+					}
+				}
+			} else {
+				if(target > A[start]) {
+					if(A[mid] > A[start]) {
+						start = mid;
+					} else {
+						end = mid;
+					}
+				} else {
+					start = mid;
+				}
+			}
+			if(end == start+1) {
+				return -1;
+			}
+		}
+		
+		return -1;
+    }
+    
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
