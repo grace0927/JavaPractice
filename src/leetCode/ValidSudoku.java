064bcb83a99486e41e53cd48dee77ebdbdaed7f0
jfeng0927 <jfeng@seawolftech.com>
2015-01-02 18:01:06 -0500

    add refer one
    valid sudoku

--- src/leetCode/ValidSudoku.java
+++ src/leetCode/ValidSudoku.java
@@ -121,6 +121,44 @@ public class ValidSudoku {
 		return true;
     }
     
+    // https://oj.leetcode.com/discuss/9216/my-solution-for-your-reference
+    public boolean isValidSudokuAlternate(char[][] board) {
+        if (board.length!=9||board[0].length!=9) {
+        	return false;
+        }
+
+        int[] row= new int[9];
+        int[] col= new int[9];
+        int[] sqr= new int[9];   
+
+        for (int i=0;i<9;i++){
+        	for (int j=0;j<9;j++){
+        		if (board[i][j]!='.') {
+                    int num = board[i][j]-'0';
+                    if ((row[i]&1<<num)>0) {
+                    	return false;
+                    } else {
+                    	row[i]|=1<<num;
+                    }
+
+                    if ((col[j]&1<<num)>0) {
+                    	return false;
+                    } else {
+                    	col[j]|=1<<num;
+                    }
+
+                    int sqr_num=(i-i%3)+j/3;
+                    if ((sqr[sqr_num]&1<<num)>0) {
+                    	return false;
+                    } else {
+                    	sqr[sqr_num]|=1<<num;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    
 	/**
 	 * @param args
 	 */
