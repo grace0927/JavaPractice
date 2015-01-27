1288915147b68f49e087644ae97a40bd717e14c0
grace0927 <cellphone2008@gmail.com>
2015-01-27 13:18:11 -0500

    add one
    implement strstr

---
+++ src/leetCode/ImplementStrStr.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package leetCode;
+
+/**
+ * @author feng
+ * https://oj.leetcode.com/problems/implement-strstr/
+ * Implement strStr().
+ * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ */
+public class ImplementStrStr {
+	public int strStr(String haystack, String needle) {
+        if(haystack.length() < needle.length()) {
+            return -1;
+        }
+        for(int i=0; i<haystack.length()-needle.length()+1; i++) {
+            if(haystack.substring(i, i+needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
