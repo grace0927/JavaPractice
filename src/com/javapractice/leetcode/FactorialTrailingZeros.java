fa6eab67763e1b1976d62f65bf004421af722241
jfeng0927 <jfeng@seawolftech.com>
2015-03-03 12:06:54 -0500

    add one
    factorial trailing zeros

---
+++ src/com/javapractice/leetcode/FactorialTrailingZeros.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.javapractice.leetcode;
+
+/**
+ * @author jianyu
+ * https://oj.leetcode.com/problems/factorial-trailing-zeroes/
+ * Given an integer n, return the number of trailing zeroes in n!.
+ * Note: Your solution should be in logarithmic time complexity.
+ *
+ */
+public class FactorialTrailingZeros {
+    public int trailingZeroes(int n) {
+        int res = 0;
+        while(n/5 > 0) {
+            res += n/5;
+            n /= 5;
+        }
+        return res;
+    }
+    
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
