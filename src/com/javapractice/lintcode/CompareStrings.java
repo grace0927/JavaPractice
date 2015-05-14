9424792c9075234a3c8aad53158875a0320f2fbb
jfeng0927 <jfeng@seawolftech.com>
2015-05-14 17:03:10 -0400

    add one
    compare string

---
+++ src/com/javapractice/lintcode/CompareStrings.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.javapractice.lintcode;
+
+/**
+ * @author jianyu
+ *
+ */
+public class CompareStrings {
+    /**
+     * @param A : A string includes Upper Case letters
+     * @param B : A string includes Upper Case letter
+     * @return :  if string A contains all of the characters in B return true else return false
+     */
+    public boolean compareStrings(String A, String B) {
+        // write your code here
+        int lenA = A.length();
+        int lenB = B.length();
+        int[] arr = new int[26];
+        
+        for(int i=0; i<lenA; i++) {
+            arr[A.charAt(i)-65]++;
+        }
+        
+        for(int i=0; i<lenB; i++) {
+            arr[B.charAt(i)-65]--;
+        }
+        
+        for(int i=0; i<26; i++) {
+            if(arr[i] < 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
