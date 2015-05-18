d8cef092d14053bf2d210e0aeb21090abb9c1b58
jfeng0927 <jfeng@seawolftech.com>
2015-05-18 16:32:39 -0400

    add one
    anagrams

--- src/com/javapractice/lintcode/Anagrams.java
+++ src/com/javapractice/lintcode/Anagrams.java
@@ -3,6 +3,11 @@
  */
 package com.javapractice.lintcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * @author jianyu
  *
@@ -37,6 +42,34 @@ public class Anagrams {
         }
         
         return true;
+    }
+    
+    /**
+     * @param strs: A list of strings
+     * @return: A list of strings
+     */
+    public List<String> anagrams(String[] strs) {
+        // write your code here
+        List<String> res = new ArrayList<>();
+        HashMap<String, String> map = new HashMap<>();
+        
+        for(int i=0; i<strs.length; i++) {
+            int[] row = new int[26];
+            for(int j=0; j<strs[i].length(); j++) {
+                row[(strs[i].charAt(j)-97)]++;
+            }
+            String key = Arrays.toString(row);
+            if(map.containsKey(key)) {
+                if(!res.contains(map.get(key))) {
+                    res.add(map.get(key));
+                }
+                res.add(strs[i]);
+            } else {
+                map.put(key, strs[i]);
+            }
+        }
+        
+        return res;
     }
     
 	/**
