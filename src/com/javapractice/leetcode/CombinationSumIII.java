9b0b88997f05ec78b2c98286274e9c0eff14658d
Feng <jfeng@seawolftech.com>
2015-08-20 17:40:22 -0400

    add one
    combination sum iii

---
+++ src/com/javapractice/leetcode/CombinationSumIII.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.javapractice.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jianyu
+ * https://leetcode.com/problems/combination-sum-iii/
+ * Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+ * Ensure that numbers within the set are sorted in ascending order.
+ * Example 1:
+ * Input: k = 3, n = 7
+ * Output:
+ * [[1,2,4]]
+ * Example 2:
+ * Input: k = 3, n = 9
+ * Output:
+ * [[1,2,6], [1,3,5], [2,3,4]]
+ *
+ */
+public class CombinationSumIII {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+        combinationSum3Util(list, row, k, n, 1);
+        return list;
+    }
+    
+    public void combinationSum3Util(List<List<Integer>> list, List<Integer> row, int k, int n, int start) {
+        if(k==1) {
+            if(n<10 && n>=start) {
+                row.add(n);
+                list.add(new ArrayList<>(row));
+                row.remove(row.size()-1);
+            }
+            return ;
+        }
+        for(int i=start; i<10; i++) {
+            row.add(i);
+            combinationSum3Util(list, row, k-1, n-i, i+1);
+            row.remove(row.size()-1);
+        }
+    }
+}
