976296ffb335e8b2e51be1ec32f3abc60bd7c56f
Feng <jfeng@seawolftech.com>
2015-08-25 14:07:18 -0400

    add one
    implement stack using queues

---
+++ src/com/javapractice/leetcode/ImplementStackUsingQueues.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.javapractice.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author jianyu
+ * https://leetcode.com/problems/implement-stack-using-queues/
+ * Implement the following operations of a stack using queues.
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * empty() -- Return whether the stack is empty.
+ * You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+ * Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), 
+ * as long as you use only standard operations of a queue.
+ * You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+ *
+ */
+public class ImplementStackUsingQueues {
+    private Queue<Integer> in = new LinkedList<>();
+    
+    // Push element x onto stack.
+    public void push(int x) {
+        Queue<Integer> temp = new LinkedList<>();
+        while(!in.isEmpty()) {
+            temp.add(in.poll());
+        }
+        in.add(x);
+        while(!temp.isEmpty()) {
+            in.add(temp.poll());
+        }
+    }
+
+    // Removes the element on top of the stack.
+    public void pop() {
+        in.poll();
+    }
+
+    // Get the top element.
+    public int top() {
+        return in.peek();
+    }
+
+    // Return whether the stack is empty.
+    public boolean empty() {
+        return in.isEmpty();
+    }
+}
