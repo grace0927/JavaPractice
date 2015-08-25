b2dfc5dd7ac14ff296efbe3f1f55b4d8509f3c6a
Feng <jfeng@seawolftech.com>
2015-08-25 14:20:15 -0400

    add one
    invert binary tree

---
+++ src/com/javapractice/leetcode/InvertBinaryTree.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.javapractice.leetcode;
+
+/**
+ * @author jianyu
+ * https://leetcode.com/problems/invert-binary-tree/
+ * Invert a binary tree.
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 
+ * to
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) {
+            return root;
+        }
+        TreeNode right = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(right);
+        return root;
+    }
+}
