ee27b7090e32943087b5f92b81b4dc4cb9d6bafe
grace0927 <cellphone2008@gmail.com>
2015-10-19 21:39:08 -0400

    add anwser

--- src/com/javapractice/cc150/Chapter118.java
+++ src/com/javapractice/cc150/Chapter118.java
@@ -30,4 +30,55 @@ public class Chapter118 {
 	public int getRankOfNumber(int x) {
 		return map.get(x);
 	}
+	
+	class RankNode {
+		public int leftSize=0;
+		public RankNode left, right;
+		public int value;
+		
+		public RankNode(int value) {
+			this.value = value;
+		}
+		
+		public void insert(int value) {
+			if(value <= this.value) {
+				// insert into left
+				if(left!=null) {
+					left.insert(value);
+				} else {
+					left = new RankNode(value);
+				}
+				leftSize++;
+			} else {
+				if(right!=null) {
+					right.insert(value);
+				} else {
+					right = new RankNode(value);
+				}
+			}
+		}
+		
+		public int getRank(int value) {
+			if(value == this.value) {
+				return leftSize;
+			} else if(value < this.value) {
+				return left!=null?left.getRank(value):-1;
+			} else {
+				return right!=null?leftSize+1+right.getRank(value):-1;
+			}
+		}
+	}
+	
+	RankNode root;
+	public void trackNode(int x) {
+		if(root==null) {
+			root = new RankNode(x);
+		} else {
+			root.insert(x);
+		}
+	}
+	
+	public int getRankOfNode(int x) {
+		return root.getRank(x);
+	}
 }
