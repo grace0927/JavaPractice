1c185bdea846a164b4535b2a8a78e8e59db4c250
Feng <jfeng@seawolftech.com>
2015-10-20 15:51:26 -0400

    add comment

--- src/com/javapractice/leetcode/BinaryTreeLevelOrderTraversalII.java
+++ src/com/javapractice/leetcode/BinaryTreeLevelOrderTraversalII.java
@@ -11,6 +11,7 @@ import java.util.Queue;
 /**
  * @author feng
  * https://oj.leetcode.com/problems/binary-tree-level-order-traversal-ii/
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
  * For example:
  * Given binary tree {3,9,20,#,#,15,7},
  * Given binary tree {3,9,20,#,#,15,7},
