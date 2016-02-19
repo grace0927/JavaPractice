27af6265ba3f0b385363ce1f9fb57bb105eb8f82
grace0927 <cellphone2008@gmail.com>
2016-02-19 18:00:49 -0500

    reform

--- src/com/javapractice/leetcode/MaximumSubarray.java
+++ src/com/javapractice/leetcode/MaximumSubarray.java
@@ -3,11 +3,13 @@ package com.javapractice.leetcode;
 /*
  * https://oj.leetcode.com/problems/maximum-subarray/
  * 
- * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+ * Find the contiguous subarray within an array (containing at least one number) 
+ * which has the largest sum.
  * For example, given the array [�?2,1,�?3,4,�?1,2,1,�?5,4],
  * the contiguous subarray [4,�?1,2,1] has the largest sum = 6.
  * More practice:
- * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ * If you have figured out the O(n) solution, try coding another solution 
+ * using the divide and conquer approach, which is more subtle.
  * 
  */
 
