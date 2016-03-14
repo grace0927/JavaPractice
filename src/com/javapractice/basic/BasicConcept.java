e9e3b09e3cbc30e4a47a04c0f1dd47c2e32cd2cb
grace0927 <cellphone2008@gmail.com>
2016-03-14 14:54:53 -0400

    redo 1
    compare version numbers

--- src/com/javapractice/basic/BasicConcept.java
+++ src/com/javapractice/basic/BasicConcept.java
@@ -3,6 +3,7 @@
  */
 package com.javapractice.basic;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 // import com.javapractice.thinking.accesscontrol.AccessControl;
@@ -62,6 +63,7 @@ public class BasicConcept {
 	public static void main(String[] args) {
 		BasicConcept test = new BasicConcept();
 		test.calculateInteger();
+		System.out.println(Arrays.toString("Welcome-to-Tutorialspoint.com".split("\\.")));
 		// AccessControl ac = new AccessControl();
 		// System.out.println(ac.a);
 	}
