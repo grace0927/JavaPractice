20d30aac202e9976b9079e5096f47e5f315433b8
grace0927 <cellphone2008@gmail.com>
2016-04-23 12:18:41 -0400

    add one
    integer break

---
+++ src/com/javapractice/leetcode/IntegerBreak.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.javapractice.leetcode;
+
+/**
+ * @author feng
+ * https://leetcode.com/problems/integer-break/
+ * Given a positive integer n, break it into the sum of at least two positive integers 
+ * and maximize the product of those integers. Return the maximum product you can get.
+ * For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4).
+ * Note: you may assume that n is not less than 2.
+ *
+ */
+public class IntegerBreak {
+    public int integerBreak(int n) {
+        if(n<6) {
+            return (n/2)*(n-n/2);
+        }
+        return (int)((n%3==1)?Math.pow(3, n/3-1)*4:(n%3==0)?Math.pow(3, n/3):Math.pow(3, n/3)*2);
+    }
+}
