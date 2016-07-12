fd05693abcee662153c22a048b2bc1a62169eb07
jfeng <cellphone2008@gmail.com>
2016-07-12 13:13:44 -0400

    add one
    water and jug problem

---
+++ src/com/javapractice/leetcode/WaterAndJugProblem.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.javapractice.leetcode;
+
+/**
+ * @author Feng
+ * https://leetcode.com/problems/water-and-jug-problem/
+ * You are given two jugs with capacities x and y litres. 
+ * There is an infinite amount of water supply available. 
+ * You need to determine whether it is possible to measure 
+ * exactly z litres using these two jugs.
+ * If z liters of water is measurable, you must have z 
+ * liters of water contained within one or both buckets by the end.
+ * Operations allowed:
+ * Fill any of the jugs completely with water.
+ * Empty any of the jugs.
+ * Pour water from one jug into another till the other jug is completely 
+ * full or the first jug itself is empty.
+ *
+ */
+public class WaterAndJugProblem {
+    public boolean canMeasureWater(int x, int y, int z) {
+        if(x+y<z) {
+            return false;
+        }
+        if(z==x || z==y || x+y==z) {
+            return true;
+        }
+        
+        while(y!=0) {
+            int tmp = y;
+            y = x%y;
+            x = tmp;
+        }
+        
+        return z%x==0;
+    }
+}
