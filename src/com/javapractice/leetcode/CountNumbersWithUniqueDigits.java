356b69a2eba54a925483eb604f1cd7e5e39ce0d5
jfeng <cellphone2008@gmail.com>
2016-07-13 11:58:51 -0400

    add one
    count numbers with unique digits

---
+++ src/com/javapractice/leetcode/CountNumbersWithUniqueDigits.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.javapractice.leetcode;
+
+/**
+ * @author Feng
+ * https://leetcode.com/problems/count-numbers-with-unique-digits/
+ * Given a non-negative integer n, count all numbers with unique digits, x, 
+ * where 0 ≤ x < 10n.
+ *
+ */
+public class CountNumbersWithUniqueDigits {
+    public int countNumbersWithUniqueDigits(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for(int i=1; i<=n; i++) {
+            dp[i] = 9;
+            int k = 9;
+            for(int j=i-1; j>0; j--) {
+                dp[i] *= (k--);
+            }
+            dp[i] += dp[i-1];
+        }
+        return dp[n];
+    }
+}
