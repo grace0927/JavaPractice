d59e515a3ce0e8ec82433766470b1f28ed3c2758
hearst-feng <jfeng@hearst.com>
2017-01-30 09:37:24 -0500

    2017-01-30
    hamming distance
    redesign list to match cvs format

---
+++ src/com/javapractice/leetcode/HammingDistance.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.javapractice.leetcode;
+
+import java.util.HashMap;
+
+/**
+ * @author jianyu
+ * https://leetcode.com/problems/hamming-distance/
+ * The Hamming distance between two integers is the number of positions 
+ * at which the corresponding bits are different.
+ * Given two integers x and y, calculate the Hamming distance.
+ * Note:
+ * 0 ≤ x, y < 231.
+ *
+ */
+public class HammingDistance {
+	public int hammingDistance(int x, int y) {
+		int z=x^y, cnt=0;
+		while(z>0) {
+			cnt += z&1;
+			z >>= 1;
+		}
+		return cnt;
+	}
+}
