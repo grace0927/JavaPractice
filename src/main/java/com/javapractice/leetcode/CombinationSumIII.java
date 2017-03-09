a4556c51bb5b572dc3e1e52a7b6d3793ac76e55f
hearst-feng <jfeng@hearst.com>
2017-03-09 09:24:36 -0500

    2017-03-09: combination sum III
    combination sum III

--- src/main/java/com/javapractice/leetcode/CombinationSumIII.java
+++ src/main/java/com/javapractice/leetcode/CombinationSumIII.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.javapractice.leetcode;
 
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author jianyu
+ * @author Jianyu Feng
  * https://leetcode.com/problems/combination-sum-iii/
  * Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
  * Ensure that numbers within the set are sorted in ascending order.
@@ -22,26 +22,46 @@ import java.util.List;
  *
  */
 public class CombinationSumIII {
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> row = new ArrayList<>();
-        combinationSum3Util(list, row, k, n, 1);
-        return list;
-    }
-    
-    public void combinationSum3Util(List<List<Integer>> list, List<Integer> row, int k, int n, int start) {
-        if(k==1) {
-            if(n<10 && n>=start) {
-                row.add(n);
-                list.add(new ArrayList<>(row));
-                row.remove(row.size()-1);
-            }
-            return ;
-        }
-        for(int i=start; i<10; i++) {
-            row.add(i);
-            combinationSum3Util(list, row, k-1, n-i, i+1);
-            row.remove(row.size()-1);
-        }
-    }
+	public List<List<Integer>> combinationSum3(int k, int n) {
+		List<List<Integer>> list = new ArrayList<>();
+		List<Integer> row = new ArrayList<>();
+
+		combinationSum3Util(list, row, k, n, 1);
+
+		return list;
+	}
+
+	public void combinationSum3Util(List<List<Integer>> list, List<Integer> row, int k, int n, int start) {
+		if(k==1) {
+			if(n<10 && n>=start) {
+				row.add(n);
+				list.add(new ArrayList<>(row));
+				row.remove(row.size()-1);
+			}
+			return ;
+		}
+
+		for(int i=start; i<10; i++) {
+			row.add(i);
+			combinationSum3Util(list, row, k-1, n-i, i+1);
+			row.remove(row.size()-1);
+		}
+	}
+
+	public void combinationSum3Helper(int k, int n, int start, List<Integer> list, List<List<Integer>> collection) {
+		if(k==0 && n==0) {
+			collection.add(new ArrayList<>(list));
+			return ;
+		}
+
+		if(n<=0 || k==0) {
+			return ;
+		}
+
+		for(int i=start; i<=9; i++) {
+			list.add(i);
+			combinationSum3Helper(k-1, n-i, i+1, list, collection);
+			list.remove(list.size()-1);
+		}
+	}
 }
