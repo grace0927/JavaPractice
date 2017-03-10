47a4983468c8527d8f2b571cbe5c0ddfc5b775c9
hearst-feng <jfeng@hearst.com>
2017-03-10 09:28:43 -0500

    2017-03-10: move zeros
    move zeros

---
+++ src/test/java/com/javapractice/leetcode/MoveZerosTest.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com.javapractice.leetcode;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+/**
+ * @author Jianyu Feng
+ *
+ */
+public class MoveZeroesTest {
+	@Test
+	public void testMoveZeroes() {
+		MoveZeroes test = new MoveZeroes();
+
+		int[] input = new int[]{ 0, 1, 0, 3, 12 };
+		int[] expected = new int[]{ 1, 3, 12, 0, 0 };
+		int[] result = test.moveZeroes(input);
+
+		assertArrayEquals(expected, result);
+	}
+}
