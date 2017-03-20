a3ce210f5e17885ea87147a085039e989060106e
hearst-feng <jfeng@hearst.com>
2017-03-20 09:43:16 -0400

    2017-03-20: next greater element I
    next greater element I

---
+++ src/test/java/com/javapractice/leetcode/NextGreaterElementITest.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com.javapractice.leetcode;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+/**
+ * @author Jianyu Feng
+ *
+ */
+public class NextGreaterElementITest {
+	@Test
+	public void testNextGreaterElements() {
+		NextGreaterElementI test = new NextGreaterElementI();
+
+		int[] nums = new int[]{ 1, 3, 4, 2 };
+		int[] findNums = new int[]{ 4, 1, 2 };
+		int[] expected = new int[]{ -1, 3, -1 };
+
+		assertArrayEquals( test.nextGreaterElements(findNums, nums), expected );
+	}
+}
