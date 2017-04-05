28b4d24de0660c7e42cb6732ea810f5efcf08f11
hearst-feng <jfeng@hearst.com>
2017-04-05 11:30:13 -0400

    2017-04-05
    same tree
    majority element
    Reconstruct Original Digits from English

--- src/main/java/com/javapractice/leetcode/MajorityElement.java
+++ src/main/java/com/javapractice/leetcode/MajorityElement.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.javapractice.leetcode;
 
@@ -9,60 +9,54 @@ import java.util.HashMap;
 /**
  * @author feng
  * https://oj.leetcode.com/problems/majority-element/
- * Given an array of size n, find the majority element. 
- * The majority element is the element that appears more 
- * than  n/2 times.
- * You may assume that the array is non-empty and 
- * the majority element always exist in the array.
+ * Given an array of size n, find the majority element.
+ * The majority element is the element that appears more than  n/2 times.
+ * You may assume that the array is non-empty and the majority element always exist in the array.
  *
  */
 public class MajorityElement {
+	public int majorityElement(int[] num) {
+		int size = num.length;
+		HashMap<Integer, Integer> count = new HashMap<>();
 
-    public int majorityElement(int[] num) {
-        int size = num.length;
-        HashMap<Integer, Integer> count = new HashMap<>();
-        
-        for(int i=0; i<size; i++) {
-            if(count.containsKey(num[i])) {
-                Integer amount = count.get(num[i]);
-                if(amount >= size/2) {
-                    return num[i];
-                } else {
-                    count.put(num[i], new Integer(amount+1));
-                }
-            } else {
-                count.put(num[i], new Integer(1));
-            }
-        }
-        return num[0];
-    }
-    
-    /*
-     * ref: http://www.programcreek.com/2014/02/leetcode-majority-element-java/
-     */
-    public int majorityElementAlter(int[] num) {
-    	if (num.length == 1) {
-    		return num[0];
-    	}
-     
-    	Arrays.sort(num);
-    	return num[num.length / 2];
-    }
-    
-    public int majorityElementMajorityVoteAlgorithm(int[] nums) {
-        int result = 0, count = 0;
-     
-        for(int i = 0; i<nums.length; i++ ) {
-            if(count == 0){
-                result = nums[ i ];
-                count = 1;
-            }else if(result == nums[i]){
-               count++;
-            }else{
-               count--;
-            }
-        }
-     
-        return result;
-    }
+		for(int i=0; i<size; i++) {
+			if(count.containsKey(num[i])) {
+				Integer amount = count.get(num[i]);
+				if(amount >= size/2) {
+					return num[i];
+				} else {
+					count.put(num[i], new Integer(amount+1));
+				}
+			} else {
+				count.put(num[i], new Integer(1));
+			}
+		}
+
+		return num[0];
+	}
+
+	/*
+	 * ref: http://www.programcreek.com/2014/02/leetcode-majority-element-java/
+	 */
+	public int majorityElementAlter(int[] num) {
+		Arrays.sort(num);
+		return num[num.length / 2];
+	}
+
+	public int majorityElementMajorityVoteAlgorithm(int[] nums) {
+		int result = 0, count = 0;
+
+		for(int i = 0; i<nums.length; i++ ) {
+			if(count == 0){
+				result = nums[ i ];
+				count = 1;
+			}else if(result == nums[i]){
+			   count++;
+			}else{
+			   count--;
+			}
+		}
+
+		return result;
+	}
 }
--- src/main/java/com/javapractice/leetcode/SameTree.java
+++ src/main/java/com/javapractice/leetcode/SameTree.java
@@ -9,27 +9,35 @@ package com.javapractice.leetcode;
 /**
  *
  * @author jianyu
- * 
+ *
  * https://oj.leetcode.com/problems/same-tree/
- * 
+ *
  * Given two binary trees, write a function to check if they are equal or not.
  * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
- * 
+ *
  */
 public class SameTree {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) {
-            return true;
-        } else if(p == null && q != null) {
-            return false;
-        } else if(p != null && q == null) {
-            return false;
-        } else {
-            if(p.val == q.val) {
-                return isSameTree(p.left, q.left)&&isSameTree(p.right, q.right);
-            } else {
-                return false;
-            }
-        }
-    }
+	public boolean isSameTreeOld(TreeNode p, TreeNode q) {
+		if(p == null && q == null) {
+			return true;
+		} else if(p == null && q != null) {
+			return false;
+		} else if(p != null && q == null) {
+			return false;
+		} else {
+			if(p.val == q.val) {
+				return isSameTree(p.left, q.left)&&isSameTree(p.right, q.right);
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p!=null && q!=null) {
+			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right) && p.val==q.val;
+		}
+
+		return p==null&&q==null;
+	}
 }
