433fcbce874a2484ff1a9c677822d19c00ecabdf
hearst-feng <jfeng@hearst.com>
2017-04-07 11:26:40 -0400

    2017-04-07: contain duplicate
    contain duplicate

--- src/main/java/com/javapractice/leetcode/ContainsDuplicate.java
+++ src/main/java/com/javapractice/leetcode/ContainsDuplicate.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.javapractice.leetcode;
 
@@ -8,21 +8,36 @@ import java.util.HashMap;
 /**
  * @author jianyu
  * https://leetcode.com/problems/contains-duplicate/
- * Given an array of integers, find if the array contains any duplicates. 
- * Your function should return true if any value appears at least twice in the array, 
+ * Given an array of integers, find if the array contains any duplicates.
+ * Your function should return true if any value appears at least twice in the array,
  * and it should return false if every element is distinct.
  *
  */
 public class ContainsDuplicate {
-    public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Boolean> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++) {
-            if(!map.containsKey(nums[i])) {
-                map.put(nums[i], true);
-            } else {
-                return true;
-            }
-        }
-        return false;
-    }
+	public boolean containsDuplicateOld(int[] nums) {
+		HashMap<Integer, Boolean> map = new HashMap<>();
+
+		for(int i=0; i<nums.length; i++) {
+			if(!map.containsKey(nums[i])) {
+				map.put(nums[i], true);
+			} else {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean containsDuplicate(int[] nums) {
+		HashSet<Integer> set = new HashSet();
+
+		for (int i:nums) {
+			if (set.contains(i)) {
+				return true;
+			}
+			set.add(i);
+		}
+
+		return false;
+	}
 }
