d68238026d40d26b6bd1d2df11b920e91aa99f9e
hearst-feng <jfeng@hearst.com>
2017-04-13 10:55:40 -0400

    2017-04-13
    missing umber
    number of boomerangs
    diameter of binary tree

--- src/main/java/com/javapractice/leetcode/MissingNumber.java
+++ src/main/java/com/javapractice/leetcode/MissingNumber.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.javapractice.leetcode;
 
@@ -13,21 +13,54 @@ package com.javapractice.leetcode;
  *
  */
 public class MissingNumber {
-    public int missingNumber(int[] nums) {
-        int pnt = 0;
-        while(pnt<nums.length) {
-            if(pnt==nums[pnt] || nums[pnt]==nums.length) {
-                pnt++;
-            } else {
-                int temp = nums[nums[pnt]];
-                nums[nums[pnt]] = nums[pnt];
-                nums[pnt] = temp;
-            }
-        }
-        pnt = 0;
-        while(pnt<nums.length && nums[pnt] == pnt) {
-            pnt++;
-        }
-        return pnt;
-    }
+	public int missingNumberOld(int[] nums) {
+		int pnt = 0;
+		while(pnt<nums.length) {
+			if(pnt==nums[pnt] || nums[pnt]==nums.length) {
+				pnt++;
+			} else {
+				int temp = nums[nums[pnt]];
+				nums[nums[pnt]] = nums[pnt];
+				nums[pnt] = temp;
+			}
+		}
+		pnt = 0;
+		while(pnt<nums.length && nums[pnt] == pnt) {
+			pnt++;
+		}
+		return pnt;
+	}
+
+	public int missingNumber(int[] nums) {
+		for (int i=0; i<nums.length; i++) {
+			while (nums[i]<nums.length && nums[nums[i]]!=nums[i]) {
+				swap(nums, i, nums[i]);
+			}
+		}
+
+		for (int i=0; i<nums.length; i++) {
+			if (nums[i]!=i) {
+				return i;
+			}
+		}
+
+		return nums.length;
+	}
+
+	public void swap(int[] nums, int i, int j) {
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
+	}
+
+	// https://discuss.leetcode.com/topic/23427/3-different-ideas-xor-sum-binary-search-java-code
+	public int missingNumberXor(int[] nums) {
+		int xor=nums.length;
+
+		for (int i=0; i<nums.length; i++) {
+			xor = xor^i^nums[i];
+		}
+
+		return xor;
+	}
 }
---
+++ src/main/java/com/javapractice/leetcode/NumberOfBoomerangs.java
@@ -0,0 +1,56 @@
+/**
+ *
+ */
+package com.javapractice.leetcode;
+
+/**
+ * @author Jianyu Feng
+ * https://leetcode.com/problems/number-of-boomerangs/
+ * Given n points in the plane that are all pairwise distinct,
+ * a "boomerang" is a tuple of points (i, j, k) such that the distance between i and j
+ * equals the distance between i and k (the order of the tuple matters).
+ * Find the number of boomerangs. You may assume that n will be at most 500 and
+ * coordinates of points are all in the range [-10000, 10000] (inclusive).
+ * Example:
+ * Input:
+ * [[0,0],[1,0],[2,0]]
+ * Output:
+ * 2
+ * Explanation:
+ * The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
+ *
+ */
+public class NumberOfBoomerangs {
+	public int numberOfBoomerangs(int[][] points) {
+		int result = 0;
+
+		for (int i=0; i<points.length; i++) {
+			HashMap<Integer, Integer> map = new HashMap<>();
+
+			for (int j=0; j<points.length; j++) {
+				int dist = distance(points, i, j);
+
+				map.put(dist, map.getOrDefault(dist, 0)+1);
+			}
+
+			result += count(map);
+		}
+
+		return result;
+	}
+
+	public int distance(int[][] points, int i, int j) {
+		int x=points[i][0]-points[j][0], y=points[i][1]-points[j][1];
+		return x*x + y*y;
+	}
+
+	public int count(HashMap<Integer, Integer> map) {
+		int result = 0;
+
+		for (int val:map.values()) {
+			result += (val * (val-1));
+		}
+
+		return result;
+	}
+}
