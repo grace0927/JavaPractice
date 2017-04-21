24d28ea2b3986cc5d9405064e57f1a7a90b2ccca
hearst-feng <jfeng@hearst.com>
2017-04-21 11:58:16 -0400

    2017-04-21
    nested iterator
    single number ii

--- src/main/java/com/javapractice/leetcode/NestedIterator.java
+++ src/main/java/com/javapractice/leetcode/NestedIterator.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.javapractice.leetcode;
 
@@ -10,42 +10,42 @@ import java.util.List;
 /**
  * @author feng
  * Given a nested list of integers, implement an iterator to flatten it.
- * Each element is either an integer, or a list -- 
+ * Each element is either an integer, or a list --
  * whose elements may also be integers or other lists.
  * Example 1:
  * Given the list [[1,1],2,[1,1]],
- * By calling next repeatedly until hasNext returns false, 
+ * By calling next repeatedly until hasNext returns false,
  * the order of elements returned by next should be: [1,1,2,1,1].
  * Example 2:
  * Given the list [1,[4,[6]]],
- * By calling next repeatedly until hasNext returns false, 
+ * By calling next repeatedly until hasNext returns false,
  * the order of elements returned by next should be: [1,4,6].
  *
  */
 public class NestedIterator implements Iterator<Integer> {
-    LinkedList<Integer> queue = new LinkedList<>();
+	LinkedList<Integer> queue = new LinkedList<>();
 
-    public NestedIterator(List<NestedInteger> nestedList) {
-        helper(nestedList);
-    }
-    
-    private void helper(List<NestedInteger> nestedList) {
-        for(NestedInteger i:nestedList) {
-            if(!i.isInteger()) {
-                helper(i.getList());
-            } else {
-                queue.add(i.getInteger());
-            }
-        }
-    }
+	public NestedIterator(List<NestedInteger> nestedList) {
+		helper(nestedList);
+	}
 
-    @Override
-    public Integer next() {
-        return queue.poll();
-    }
+	private void helper(List<NestedInteger> nestedList) {
+		for(NestedInteger i:nestedList) {
+			if(!i.isInteger()) {
+				helper(i.getList());
+			} else {
+				queue.add(i.getInteger());
+			}
+		}
+	}
 
-    @Override
-    public boolean hasNext() {
-        return !queue.isEmpty();
-    }
+	@Override
+	public Integer next() {
+		return queue.poll();
+	}
+
+	@Override
+	public boolean hasNext() {
+		return !queue.isEmpty();
+	}
 }
--- src/main/java/com/javapractice/leetcode/SingleNumberII.java
+++ src/main/java/com/javapractice/leetcode/SingleNumberII.java
@@ -5,24 +5,48 @@ package com.javapractice.leetcode;
  * Given an array of integers, every element appears three times except for one. Find that single one.
  * Note:
  * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
- * 
+ *
  */
 
 public class SingleNumberII {
-
 	public int singleNumber(int[] A) {
-        int size = A.length;
-        int[] bin = new int[32];
-        int result=0;
-        
-        for(int i=0; i<32; i++) {
-        	for(int j=0; j<size; j++) {
-        		bin[i] += (A[j]>>i & 1);
-        	}
-        	result |= ((bin[i]%3)<<i);
-        }
-        
-        return result;
-    }
-	
+		int size = A.length;
+		int[] bin = new int[32];
+		int result=0;
+
+		for(int i=0; i<32; i++) {
+			for(int j=0; j<size; j++) {
+				bin[i] += (A[j]>>i & 1);
+			}
+			result |= ((bin[i]%3)<<i);
+		}
+
+		return result;
+	}
+
+	public int singleNumberHashMap(int[] nums) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		for (int i:nums) {
+			map.put(i, map.getOrDefault(i, 0)+1);
+		}
+
+		for (int i:map.keySet()) {
+			if (map.get(i)==1) {
+				return i;
+			}
+		}
+
+		return 0;
+	}
+
+	//https://discuss.leetcode.com/topic/11877/detailed-explanation-and-generalization-of-the-bitwise-operation-method-for-single-numbers
+	public int singleNumberBitwise(int[] A) {
+		int ones = 0, twos = 0;
+		for(int i = 0; i < A.length; i++){
+			ones = (ones ^ A[i]) & ~twos;
+			twos = (twos ^ A[i]) & ~ones;
+		}
+		return ones;
+	}
 }
