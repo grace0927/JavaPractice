fdbe45964f9d3cdc9e19a24dcc965e537f67726b
hearst-feng <jfeng@hearst.com>
2017-05-17 09:27:43 -0400

    2017-05-17
    subtree of another tree
    happy number
    power of two
    remove duplicates from sorted list

--- src/main/java/com/javapractice/leetcode/HappyNumber.java
+++ src/main/java/com/javapractice/leetcode/HappyNumber.java
@@ -1,18 +1,19 @@
 /**
- * 
+ *
  */
 package com.javapractice.leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
- * @author jianyu
+ * @author Jianyu Feng
  * https://leetcode.com/problems/happy-number/
  * Write an algorithm to determine if a number is "happy".
- * A happy number is a number defined by the following process: Starting with any positive integer, 
- * replace the number by the sum of the squares of its digits, and repeat the 
- * process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. 
- * Those numbers for which this process ends in 1 are happy 
+ * A happy number is a number defined by the following process: Starting with any positive integer,
+ * replace the number by the sum of the squares of its digits, and repeat the
+ * process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+ * Those numbers for which this process ends in 1 are happy
  * numbers.
  * Example: 19 is a happy number
  * 1^2 + 9^2 = 82
@@ -22,21 +23,21 @@ import java.util.HashMap;
  *
  */
 public class HappyNumber {
-    public boolean isHappy(int n) {
-        HashMap<Integer, Boolean> map = new HashMap<>();
-		
+	public boolean isHappyMap(int n) {
+		HashMap<Integer, Boolean> map = new HashMap<>();
+
 		while(!map.containsKey(n)) {
 			if(n==1){
-			    map.put(n, true);
+				map.put(n, true);
 			} else {
-			    map.put(n, false);
+				map.put(n, false);
 			}
 			n = sum(n);
 		}
-		
+
 		return map.get(n);
-    }
-	
+	}
+
 	public int sum(int n) {
 		int sum = 0;
 		while(n>=10) {
@@ -45,5 +46,28 @@ public class HappyNumber {
 		}
 		sum += n*n;
 		return sum;
-    }
+	}
+
+	public boolean isHappy(int n) {
+		HashSet<Integer> set = new HashSet<>();
+		set.add(n);
+		while (n!=1) {
+			n=next(n);
+			if (set.contains(n)) {
+				return false;
+			}
+			set.add(n);
+		}
+		return true;
+	}
+
+	private int next(int n) {
+		int sum = 0;
+		while (n>0) {
+			sum += (n%10)*(n%10);
+			n /= 10;
+		}
+
+		return sum;
+	}
 }
