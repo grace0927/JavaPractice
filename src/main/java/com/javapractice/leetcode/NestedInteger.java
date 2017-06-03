e61a7b8af816815fd16f8a11f04e7c8b618a14aa
grace0927 <cellphone2008@gmail.com>
2017-06-03 10:31:47 -0400

    fix bugs

--- src/main/java/com/javapractice/leetcode/NestedInteger.java
+++ src/main/java/com/javapractice/leetcode/NestedInteger.java
@@ -3,33 +3,48 @@
  */
 package com.javapractice.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author feng
+ * @author Jianyu Feng
  *
  */
-public interface NestedInteger {
+public class NestedInteger {
 	// Constructor initializes an empty nested list.
-	public NestedInteger();
+	public NestedInteger() {
+		
+	}
 
 	// Constructor initializes a single integer.
-	public NestedInteger(int value);
+	public NestedInteger(int value){
+		
+	}
 
 	// @return true if this NestedInteger holds a single integer, rather than a nested list.
-	public boolean isInteger();
+	public boolean isInteger(){
+		return true;
+	}
 
 	// @return the single integer that this NestedInteger holds, if it holds a single integer
 	// Return null if this NestedInteger holds a nested list
-	public Integer getInteger();
+	public Integer getInteger(){
+		return new Integer(1);
+	}
 
 	// Set this NestedInteger to hold a single integer.
-	public void setInteger(int value);
+	public void setInteger(int value){
+		
+	}
 
 	// @return the nested list that this NestedInteger holds, if it holds a nested list
 	// Return null if this NestedInteger holds a single integer
-	public List<NestedInteger> getList();
+	public List<NestedInteger> getList(){
+		return new ArrayList<>();
+	}
 
 	// Set this NestedInteger to hold a nested list and adds a nested integer to it.
-	public void add(NestedInteger ni);
+	public void add(NestedInteger ni){
+		
+	}
 }
