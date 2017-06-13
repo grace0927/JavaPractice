d1e970a3396e71be0ec63434081897b91e3291bd
hearst-feng <jfeng@hearst.com>
2017-06-13 09:44:43 -0400

    2017-06-13
    binary tree level order traversal ii
    search insert position

--- src/main/java/com/javapractice/leetcode/BinaryTreeLevelOrderTraversalII.java
+++ src/main/java/com/javapractice/leetcode/BinaryTreeLevelOrderTraversalII.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.javapractice.leetcode;
 
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Queue;
 
 /**
- * @author feng
+ * @author Jianyu Feng
  * https://oj.leetcode.com/problems/binary-tree-level-order-traversal-ii/
  * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
  * For example:
@@ -29,32 +29,33 @@ import java.util.Queue;
  *
  */
 public class BinaryTreeLevelOrderTraversalII {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        Queue<TreeNode> rowQueue = new LinkedList<>();
-        if(root == null) {
-            return result;
-        }
-        rowQueue.add(root);
-        while(!rowQueue.isEmpty()) {
-            List<Integer> row = new ArrayList<>();
-            for(TreeNode node:rowQueue) {
-                row.add(node.val);
-            }
-            result.add(0, row);
-            Queue<TreeNode> temp = new LinkedList<>();
-            while(!rowQueue.isEmpty()) {
-                TreeNode tempNode = rowQueue.poll();
-                if(tempNode.left != null) {
-                    temp.add(tempNode.left);
-                }
-                if(tempNode.right != null) {
-                    temp.add(tempNode.right);
-                }
-            }
-            rowQueue = temp;
-        }
-        return result;
-    }
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<>();
+
+		if(root != null) {
+			Queue<TreeNode> rowQueue = new LinkedList<>();
+			rowQueue.add(root);
+			while(!rowQueue.isEmpty()) {
+				List<Integer> row = new ArrayList<>();
+				for(TreeNode node:rowQueue) {
+					row.add(node.val);
+				}
+				result.add(0, row);
+				Queue<TreeNode> temp = new LinkedList<>();
+				while(!rowQueue.isEmpty()) {
+					TreeNode tempNode = rowQueue.poll();
+					if(tempNode.left != null) {
+						temp.add(tempNode.left);
+					}
+					if(tempNode.right != null) {
+						temp.add(tempNode.right);
+					}
+				}
+				rowQueue = temp;
+			}
+		}
+
+		return result;
+	}
 
 }
